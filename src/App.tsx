4fe17e06fbcd56988d2a1f791d75155691304f79
gabebastarz1 <gabrielbastarz.tggt@gmail.com>
2024-11-13 08:37:25 -0300

    nomes em PTBR e app.tsx certinho

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import HomeAtleta from './pages/HomeAtleta'
-
+import Chamada from './pages/Chamada';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="">
-      <HomeAtleta />
+      <Outlet/>
     </div>
   );
 }
